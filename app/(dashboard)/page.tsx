1901ccdb37548448c81fa4bb38da5a35d698fa47
menachem64 <mnsudak02@gmail.com>
2024-07-30 14:36:01 +0300

    09:58

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,7 +3,7 @@ import { DataGrid } from "@/components/data-grid";
 export default function Home() {
 
   return (
-    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-20">
        <DataGrid/>
     </div>
   );
