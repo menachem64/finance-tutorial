11ad5aa50d3bd8519fb66aeece75f62767bb8268
menachem64 <mnsudak02@gmail.com>
2024-08-01 11:13:58 +0300

    fix the filter by data-table

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -75,14 +75,14 @@ const [ConfirmDialog, confirm] = useConfirm(
     <div>
       <ConfirmDialog/>
           <div className="flex items-center py-4">
-        <Input
-          placeholder={`Filter ${filterKey}...`}
-          value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
+          <Input
+             placeholder={`Filter ${filterKey}...`}
+             value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
+             onChange={(event) =>
+               table.getColumn(filterKey)?.setFilterValue(event.target.value)
+             }
+             className="max-w-sm"
+          />
         {table.getFilteredSelectedRowModel().rows.length > 0 && (
             <Button
             disabled={disabled}
